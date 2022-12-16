34cd2dcf03b28a4abfe7504947310c82e3d8e96a
Facundo Noya <facunoya@hotmail.com>
2022-12-16 20:56:18 -0300

    iniciando prueba

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(express.static('public'))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+
+})
+app.get("/login", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+
+
+
+app.listen(port, ()=> console.log(`Servidor corriendo en el puerto ${port}`))
